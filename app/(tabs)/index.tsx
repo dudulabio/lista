10b1a80668dcb0e6b81715f659c9adaeca7ee848
Eduardo de Lábio <dudulabio@gmail.com>
2024-10-10 15:25:25 -0400

    first commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,17 @@
+
+
+
+
+
+import React from 'react';
+import{SafeAreaView} from 'react-native';
+import Home from '../../screens/Home';
+
+
+export default function App() {
+  return(
+    <SafeAreaView style={{flex:2}}> 
+     <Home/>
+    </SafeAreaView>
+  );
+}
---
+++ utils/generateId.tsx
@@ -0,0 +1,5 @@
+
+
+
+
+export const generateId =() => Math.random().toString(36).substr(2,9);
